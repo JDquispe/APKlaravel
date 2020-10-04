b3697129cd6871b1fa4407e8b89b66a493736599
JDquispe <jesusquispecastro@gmail.com>
2020-10-03 23:40:58 -0500

    Initial commit

---
+++ app/src/main/java/com/xcheko51x/ejemplo_retrofit/AdaptadorRetrofit.java
@@ -0,0 +1,18 @@
+package com.xcheko51x.ejemplo_retrofit;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class AdaptadorRetrofit {
+    Retrofit retrofit;
+
+    public AdaptadorRetrofit(){ }
+
+    public Retrofit getAdapter(){
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.0.103:8008/PruebasQuispe/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return retrofit;
+    }
+
+}
